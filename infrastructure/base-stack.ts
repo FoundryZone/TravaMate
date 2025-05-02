2d62ba731a11f223ace54d16ced6377322a65bd5
bkadu3 <bkadu3@gmail.com>
2025-05-02 11:42:38 +0530

    TRAVAMATE-3

--- infrastructure/base-stack.ts
+++ infrastructure/base-stack.ts
@@ -1,13 +1,13 @@
 import { CfnElement, Stack, StackProps } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import { Construct, Node } from "constructs";
+
 export interface MyStackProps extends cdk.StackProps {
   suffix: string;
 }
-let myProps: MyStackProps | undefined = undefined;
+
 export class BaseStack extends Stack {
   constructor(scope: Construct, id: string, props?: MyStackProps) {
     super(scope, id, props);
-    myProps = props;
   }
 }
