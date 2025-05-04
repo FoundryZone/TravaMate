bdf21a82c020673fcc27f1321341506d05f1f00b
bkadu3 <bkadu3@gmail.com>
2025-05-04 13:46:29 +0530

    TM-4 ongoing commit

--- infrastructure/bin/trava_mate_infra.ts
+++ infrastructure/bin/trava_mate_infra.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { TravaMateInfraStack } from "../src/trava_mate_booking_agent-stack";
-import { TravMateAgentBookingLambdaStack } from "../src/trava_mate_agent_booking_lambda-stack";
+import { TravaMateInfraStack } from "../src/trava-mate-booking-agent-stack";
+import { TravMateAgentBookingLambdaStack } from "../src/trava-mate-booking-lambda-stack";
 
 const app = new cdk.App();
 const suffixProvided = app.node.tryGetContext("suffix");
