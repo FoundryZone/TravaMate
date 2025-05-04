8fd9da89dc2b7674b59ae0a88f502df48087da01
bkadu3 <bkadu3@gmail.com>
2025-05-04 17:52:01 +0530

    TM-4 ongoing commit

--- infrastructure/bin/trava_mate_infra.ts
+++ infrastructure/bin/trava_mate_infra.ts
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { TravaMateInfraStack } from "../src/trava-mate-booking-agent-stack";
-import { TravMateAgentBookingLambdaStack } from "../src/trava-mate-booking-lambda-stack";
+import { TravaMateBookingAgentStack } from "../src/booking-agent/trava-mate-booking-agent-stack";
+import { TravMateAgentBookingLambdaStack } from "../src/booking-agent/trava-mate-booking-lambda-stack";
 
 const app = new cdk.App();
 const suffixProvided = app.node.tryGetContext("suffix");
 const suffix = suffixProvided ? `-${suffixProvided}` : "";
-const stackProps = { suffix: suffix };
-
-new TravaMateInfraStack(app, `TravaMateInfraStack${suffix}`, stackProps);
-new TravMateAgentBookingLambdaStack(app, `TravMateAgentBookingLambdaStack${suffix}`, stackProps);
+const stackProps = {
+  suffix: suffix,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+};
+const bookingLambdaStack = new TravMateAgentBookingLambdaStack(
+  app,
+  `TravMateAgentBookingLambdaStack${suffix}`,
+  stackProps
+);
+new TravaMateBookingAgentStack(
+  app,
+  bookingLambdaStack.bookingLambda,
+  `TravaMateBookingAgentStack${suffix}`,
+  stackProps
+);
---
+++ infrastructure/src/booking-agent/trava-mate-booking-lambda-stack.ts
@@ -0,0 +1,49 @@
+import { Duration, CfnOutput } from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+import { BaseStack, MyStackProps } from "../../base-stack";
+import * as path from "node:path";
+
+export class TravMateAgentBookingLambdaStack extends BaseStack {
+  public bookingLambda: lambda.Function;
+
+  constructor(scope: Construct, id: string, props?: MyStackProps) {
+    super(scope, id, props);
+
+    this.bookingLambda = new lambda.Function(this, "BookingLambda", {
+      functionName: `BookingLambda${props?.suffix}`,
+      runtime: lambda.Runtime.NODEJS_22_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler")),
+      handler: "index.handler",
+      timeout: Duration.seconds(30),
+      memorySize: 128,
+    });
+
+    new CfnOutput(this, "BookingLambdaARN", {
+      value: this.bookingLambda.functionArn,
+    });
+
+    var addExecuttionPolicy = this.bookingLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["bedrock:*"],
+        resources: ["*"],
+      })
+    );
+
+    // this.bookingLambda.role?.addManagedPolicy(
+    //   iam.ManagedPolicy.fromManagedPolicyName(
+    //     this,
+    //     "AmazonS3FullAccess",
+    //     "AmazonS3FullAccess"
+    //   )
+    // );
+
+    this.bookingLambda.addPermission("AllowBedrock", {
+      principal: new iam.ServicePrincipal("bedrock.amazonaws.com"),
+      action: "lambda:InvokeFunction",
+      sourceArn: `arn:aws:bedrock:${props?.env?.region}:${props?.env?.account}:agent/*`,
+    });
+  }
+}
